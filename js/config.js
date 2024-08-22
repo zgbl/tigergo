5cbbc91a63cb3b63accb1b0ef6b22c3c1c0d8165
Xinyu Tu <carson_tu@hotmail.com>
2024-08-22 13:36:09 -0400

    dd

--- js/config.js
+++ js/config.js
@@ -1,5 +1,6 @@
 // config.js
 const CONFIG = {
     API_BASE_URL: "https://black-rice-go-serverless6.vercel.app",
+    API_VERCEL_NEXTJS_BASE_URL: "https://blackricegobackend2-nextjs-oi9ggawvz-xtus-projects.vercel.app",
   };
   
