ad9a0951202480cb5349c4fac129eb2cb99e241b
Xinyu Tu <carson_tu@hotmail.com>
2025-07-31 15:39:52 -0400

    update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,15 @@
 // config.js
+//const CONFIG = {
+//    API_BASE_URL: "https://black-rice-go-serverless6.vercel.app",
+//    API_VERCEL_NEXTJS_BASE_URL: "https://blackricegobackend2-nextjs.vercel.app",
+//    GITHUB_PAGE_FORUM_URL: "https://zgbl.github.io/tigergo/Forum11.html",
+//    FORUM_POST_ENDPOINT: "/forum/post",
+//  };
+
 const CONFIG = {
-    API_BASE_URL: "https://black-rice-go-serverless6.vercel.app",
+    // 把旧的改为新的backend
+    API_BASE_URL: "https://blackricegobackend2-nextjs.vercel.app",
     API_VERCEL_NEXTJS_BASE_URL: "https://blackricegobackend2-nextjs.vercel.app",
     GITHUB_PAGE_FORUM_URL: "https://zgbl.github.io/tigergo/Forum11.html",
     FORUM_POST_ENDPOINT: "/forum/post",
-  };
+};
